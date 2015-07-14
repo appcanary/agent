bd0d9441323a02d681b90ef507c9a5ffb2afc03c
Phill MV <phillmv@okayfail.com>
2015-07-14 16:26:08 -0700

    Interim poking around on prune.rb

--- package/prune.rb
+++ package/prune.rb
@@ -9,26 +9,35 @@ require 'time'
 API_TOKEN = JSON.parse(`cat ~/.packagecloud`)["token"]
 USER_REPO = ARGV[0]
 
-base_url = "https://#{API_TOKEN}:@packagecloud.io/api/v1/repos/#{USER_REPO}"
+@base_url = "https://#{API_TOKEN}:@packagecloud.io/api/v1/repos/#{USER_REPO}"
 
 package_url = "/packages.json"
 
-url = base_url + package_url
+url = @base_url + package_url
 
-all_pkg = RestClient.get(url)
-
+all_pkg = RestClient.get(url);
 pkgs =  JSON.parse(all_pkg)
 
-pkgs.each do |p|
+# prune everything
+# pkgs.each do |p|
+#   distro_version = p["distro_version"]
+#   filename = p["filename"]
+#   yank_url = "/#{distro_version}/#{filename}"
+#   url = base_url + yank_url
+#   puts "yanking #{url}"
+#  
+#   result = RestClient.delete(url)
+#   if result == {}
+#     puts "successfully yanked #{filename}!"
+#   end
+# end
+
+
+def yank_url(p)
   distro_version = p["distro_version"]
   filename = p["filename"]
   yank_url = "/#{distro_version}/#{filename}"
-  url = base_url + yank_url
-  puts "yanking #{url}"
- 
-  result = RestClient.delete(url)
-  if result == {}
-    puts "successfully yanked #{filename}!"
-  end
+  @base_url + yank_url
 end
 
+pkgs.select { |p| p["version"] < "0.0.2" }.map { |p| RestClient.delete yank_url(p) }
