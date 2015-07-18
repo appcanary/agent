df620a118813256cd307976c75f83fd93720ab55
Mike MacLeod <mikemacleod@gmail.com>
2015-07-18 12:28:50 -0400

    Touch and chown /var/log/appcanary.log

--- package/debian/wheezy/post-install.sh
+++ package/debian/wheezy/post-install.sh
@@ -4,3 +4,5 @@ id appcanary > /dev/null 2>&1
 if [ $? == 1 ]; then
   useradd -r -d /var/db/appcanary -s /sbin/nologin -c "AppCanary Agent" appcanary
 fi
+touch /var/log/appcanary.log
+chown appcanary:appcanary /var/log/appcanary.log
