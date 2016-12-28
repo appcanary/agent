fdccbc3a86815fbc509cf219319ff727e19811f5
J Irving <j@lollyshouse.ca>
2016-12-28 17:23:45 -0500

    Add a test

--- agent/upgrade_test.go
+++ agent/upgrade_test.go
@@ -22,7 +22,7 @@ func TestBuildDebianUpgrade(t *testing.T) {
 	assert.Equal("foobar", lastArg)
 }
 
-func TestBuildCentOSUpgrade(t *testing.T) {
+func TestBuildCentOSUpgradeWithSuffixedVersion(t *testing.T) {
 	assert := assert.New(t)
 
 	packageList := map[string]string{"foobar": "foobar-version-with-suffix.rpm"}
@@ -35,4 +35,19 @@ func TestBuildCentOSUpgrade(t *testing.T) {
 	lastArg := upgradeArgs[len(upgradeArgs)-1]
 
 	assert.Equal("foobar-version-with-suffix", lastArg)
+}
+
+func TestBuildCentOSUpgradeWithoutSuffixedVersion(t *testing.T) {
+	assert := assert.New(t)
+
+	packageList := map[string]string{"foobar": "foobar-version-without-suffix"}
+	commands := buildCentOSUpgrade(packageList)
+
+	assert.Equal(1, len(commands))
+	assert.Equal("yum", commands[0].Name)
+
+	upgradeArgs := commands[0].Args
+	lastArg := upgradeArgs[len(upgradeArgs)-1]
+
+	assert.Equal("foobar-version-without-suffix", lastArg)
 }
